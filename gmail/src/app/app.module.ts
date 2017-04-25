c5fd8bc03499847961355b5bd1eedd17a60440ee
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-04-25 17:42:19 +0200

    changes upload

--- gmail/src/app/app.module.ts
+++ gmail/src/app/app.module.ts
@@ -7,10 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { Inbox } from '../pages/inbox/inbox';
+
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, Inbox
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage, Inbox
   ],
   providers: [
     StatusBar,
